edacb4e918a3bdb0c51f42b23b076bd88c606731
vitiksha <gamitvitiksha02@gmail.com>
2022-08-12 23:36:13 +0530

    React-Task

--- React_Task_SP/task/src/store.js
+++ React_Task_SP/task/src/store.js
@@ -11,8 +11,6 @@ export default configureStore;
 function logger({ getState }) {
   return (next) => (action) => {
     console.log("will dispatch", action);
-
-    // Call the next dispatch method in the middleware chain.
     const returnValue = next(action);
 
     console.log("state after dispatch", getState());
